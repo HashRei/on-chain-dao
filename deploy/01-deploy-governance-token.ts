f22034dfb289e4f037ddf9d0f1e4194425ca0b94
HashRei <hashrei@protonmail.com>
2022-05-28 12:02:39 +0200

    Add governance token deploy

---
+++ deploy/01-deploy-governance-token.ts
@@ -0,0 +1,27 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/dist/types"
+import { ethers } from "hardhat"
+
+const deployGovernanceToken: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { getNamedAccounts, deployments } = hre
+  const { deploy, log } = deployments
+  const { deployer } = await getNamedAccounts()
+  log("----------------------------------------------------")
+  log("Deploying GovernanceToken and waiting for confirmations...")
+  const governanceToken = await deploy("GovernanceToken", {
+    from: deployer,
+    args: [],
+    log: true, // logs will be printed out
+  })
+  log(`GovernanceToken at ${governanceToken.address}`)
+  log(`Delegating to ${deployer}`)
+  await delegate(governanceToken.address, deployer)
+  log("Delegated!")
+}
+
+const delegate = async (governanceTokenAddress: string, delegatedAccount: string) => {
+  const governanceToken = await ethers.getContractAt("GovernanceToken", governanceTokenAddress)
+  const transactionResponse = await governanceToken.delegate(delegatedAccount)
+  await transactionResponse.wait(1)
+}
+export default deployGovernanceToken
