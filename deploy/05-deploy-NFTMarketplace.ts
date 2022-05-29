b5f0d21c2c06dc6815dc25c5ab823a2b51c5006c
HashRei <hashrei@protonmail.com>
2022-05-29 13:07:16 +0200

    Add NFTMarketplace deploy

---
+++ deploy/05-deploy-NFTMarketplace.ts
@@ -0,0 +1,23 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/dist/types"
+import { ethers } from "hardhat"
+
+const deployNFTMarketplace: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    const { getNamedAccounts, deployments } = hre
+    const { log, deploy } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    log("----------------------------------------------------")
+    log("Deploying NFTMarketplace and waiting for confirmations...")
+    const nftMarketplace = await deploy("NFTMarketplace", {
+        from: deployer,
+        args: [],
+        log: true, // logs will be printed out
+  })
+  log(`NFTMarketplace at ${nftMarketplace.address}`)
+  const nftMarketplaceContract = await ethers.getContractAt("NFTMarketplace", nftMarketplace.address)
+  const timeLock = await ethers.getContract("TimeLock")
+  const transferTx = await nftMarketplaceContract.transferOwnership(timeLock.address)
+  await transferTx.wait(1)
+}
+export default deployNFTMarketplace
