d4f17d8e620971a1d355b56faf5508e31a6c27e3
HashRei <hashrei@protonmail.com>
2022-06-01 18:02:17 +0200

    Update VOTING_PERIOD value

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -1,6 +1,6 @@
 export const MIN_DELAY = 3600;
 export const QUORUM_PERCENTAGE = 4; // Need 4% of voters to pass
-export const VOTING_PERIOD = 1; // blocks
+export const VOTING_PERIOD = 5; // blocks
 export const VOTING_DELAY = 1; // 1 Block - How many blocks till a proposal vote becomes active
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
 
