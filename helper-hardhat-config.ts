e8acf777ffa6f63599965c26f9b53e234e403a92
HashRei <hashrei@protonmail.com>
2022-06-01 17:11:51 +0200

    Update helper-harhat-config

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -1,11 +1,13 @@
 export const MIN_DELAY = 3600;
 export const QUORUM_PERCENTAGE = 4; // Need 4% of voters to pass
-export const VOTING_PERIOD = 5; // blocks
+export const VOTING_PERIOD = 1; // blocks
 export const VOTING_DELAY = 1; // 1 Block - How many blocks till a proposal vote becomes active
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
 
 export const CREATE_FUNC = "createToken";
 export const CREATE_PROPOSAL_DESCRIPTION = "Proposal #1 Create a new monkey NFT";
+export const CREATE_REASON = "It is mandatory to have a monkey NFT"
 
 export const BURN_FUNC = "burn";
 export const BURN_PROPOSAL_DESCRIPTION = "Proposal #2 Burn the monkey NFT";
+export const BURN_REASON = "Who cares about a monkey NFT"
