5c083c193209c81c14d9def6376dd534ef3466a4
HashRei <hashrei@protonmail.com>
2022-06-02 10:59:01 +0200

    Update hardhat.config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,26 +1,38 @@
-import * as dotenv from "dotenv";
+import * as dotenv from "dotenv"
 
-import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "@nomiclabs/hardhat-ethers";
-import "hardhat-deploy";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "hardhat-contract-sizer";
-import "solidity-coverage";
+import { HardhatUserConfig, task } from "hardhat/config"
+import "@nomiclabs/hardhat-etherscan"
+import "@nomiclabs/hardhat-waffle"
+import "@nomiclabs/hardhat-ethers"
+import "hardhat-deploy"
+import "@typechain/hardhat"
+import "hardhat-gas-reporter"
+import "hardhat-contract-sizer"
+import "solidity-coverage"
+import fs from "fs";
 
-dotenv.config();
+dotenv.config()
+
+
+const infuraProjectId = fs.existsSync(".infuraprojectid")
+? fs.readFileSync(".infuraprojectid", "utf-8").trim()
+: "";
+if (!infuraProjectId) console.log("Missing infura project id");
+
+const privateKey = fs.existsSync("../secret.txt")
+? fs.readFileSync("../secret.txt", "utf-8").trim()
+: "";
+if (!privateKey) console.log("Missing private key");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+  const accounts = await hre.ethers.getSigners()
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(account.address)
   }
-});
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -32,33 +44,39 @@ const config: HardhatUserConfig = {
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200,
-      },
-    },
+        runs: 200
+      }
+    }
   },
   namedAccounts: {
     deployer: {
-      default: 0,
+      default: 0
     },
   },
   networks: {
     hardhat: {
-      chainId: 31337,
+      chainId: 31337
     },
     localHost: {
       url: "",
-      chainId: 31337,
+      chainId: 31337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${infuraProjectId}`,
+      chainId: 80001,
+      gasPrice: 14000000000,
+      accounts: [privateKey],
     },
   },
   gasReporter: {
     enabled: true,
     currency: "USD",
     outputFile: "gas-report.txt",
-    noColors: true,
+    noColors: true
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
-};
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
+}
 
-export default config;
+export default config
